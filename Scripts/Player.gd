f677a66629e0ea272c6b7acda036d420546c744c
Androo <andrew.coon84@gmail.com>
2022-11-05 22:05:08 -0600

    full movement

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -10,7 +10,7 @@ var sense = .1
 var snap
 
 var direction = Vector3()
-var velocity = Vector3()
+var vel = Vector3()
 var gravityVector = Vector3()
 var movement = Vector3()
 
@@ -33,6 +33,24 @@ func _input(event: InputEvent) -> void:
 func _physics_process(delta: float) -> void:
 	direction = Vector3.ZERO
 	var horizontalRot = global_transform.basis.get_euler().y
-	var forwardInput = Input.get_action_strength()
+	var forwardInput = Input.get_action_strength("moveBack") - Input.get_action_strength("moveForward")
+	var horizontalInput = Input.get_action_strength("moveRight") - Input.get_action_strength("moveLeft")
+	direction = Vector3(horizontalInput, 0, forwardInput).rotated(Vector3.UP, horizontalRot).normalized()
+	
+	if is_on_floor():
+		snap = -get_floor_normal()
+		gravityVector = Vector3.ZERO
+	else:
+		snap = Vector3.DOWN
+		gravityVector += Vector3.DOWN * gravity * delta
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		snap = Vector3.ZERO
+		gravityVector = Vector3.UP * jump
+	
+	vel = vel.linear_interpolate(direction*speed, delta)
+	movement = vel + gravityVector
+	
+	move_and_slide_with_snap(movement, snap, Vector3.UP)
 	pass
 
