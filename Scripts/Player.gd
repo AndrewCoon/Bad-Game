6363981812c75164dfbc876caa8ebb3a861ce90d
Chinmay Tiwari <26Chinmay.Tiwari@boiseschools.net>
2022-11-07 09:56:42 -0700

    ha

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -6,7 +6,7 @@ var jump = 7
 
 var cameraAccel = 50
 var sense = .1
-
+var frick = 0.61
 var snap
 
 var direction = Vector3()
@@ -16,6 +16,7 @@ var movement = Vector3()
 
 onready var head = $Head
 onready var camera = $Head/Camera
+onready var gun = $barel
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -27,30 +28,35 @@ func _input(event: InputEvent) -> void:
 		head.rotate_x(deg2rad(-event.relative.y * sense))
 		
 		head.rotation.x = clamp(head.rotation.x, deg2rad(-90), deg2rad(90))	
-		
+		gun.rotation.x = head.rotation.x * -1
+	if event is InputEventMouseButton:
+		#gun.rotate_x(0.3)
+		pass
+		#boi._integrate_forces(set_angular_velocity(value))
 	pass
 	
 func _physics_process(delta: float) -> void:
 	direction = Vector3.ZERO
 	var horizontalRot = global_transform.basis.get_euler().y
+	
 	var forwardInput = Input.get_action_strength("moveBack") - Input.get_action_strength("moveForward")
 	var horizontalInput = Input.get_action_strength("moveRight") - Input.get_action_strength("moveLeft")
 	direction = Vector3(horizontalInput, 0, forwardInput).rotated(Vector3.UP, horizontalRot).normalized()
-	
-	if is_on_floor():
-		snap = -get_floor_normal()
-		gravityVector = Vector3.ZERO
-	else:
-		snap = Vector3.DOWN
-		gravityVector += Vector3.DOWN * gravity * delta
-	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		snap = Vector3.ZERO
-		gravityVector = Vector3.UP * jump
-	
-	vel = vel.linear_interpolate(direction*speed, delta)
-	movement = vel + gravityVector
-	
-	move_and_slide_with_snap(movement, snap, Vector3.UP)
+	if(forwardInput != 0 or horizontalInput != 0):
+		if is_on_floor():
+			snap = -get_floor_normal()
+			gravityVector = Vector3.ZERO
+			
+		else:
+			snap = Vector3.DOWN
+			gravityVector += Vector3.DOWN * gravity * delta
+		
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			snap = Vector3.ZERO
+			gravityVector = Vector3.UP * jump
+		
+		vel = vel.linear_interpolate(direction*speed, delta)
+		movement = vel + gravityVector
+		move_and_slide_with_snap(movement, snap, Vector3.UP)
 	pass
 
